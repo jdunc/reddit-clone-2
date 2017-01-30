5783b0d69ca1be0adfb6571158c0d70411a84264
Jordan Duncan <j.duncan@rochester.edu>
2017-01-30 14:11:12 -0600

    finished with angular routing

--- public/app.config.js
+++ public/app.config.js
@@ -1,32 +1,33 @@
-(function () {
-  const myApp = angular.module('app', ['ui.router']);
+(function() {
+    const myApp = angular.module('app', ['ui.router']);
 
-  // myApp.config(config(['$stateProvider', '$locationProvider', '$urlRouterProvider']));
-  // function config($stateProvider, $locationProvider, $urlRouterProvider) {
-  //   $locationProvider
-  //   .html5Mode(true) // enable html5Mode for pushstate ('#'-less URLs DOESN'T WORK)
-  //   .hashPrefix('!');
-  myApp.config(config);
-  function config($stateProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false,
-    });
-    const reddit = {
-      name: 'reddit',
-      url: '/',
-      template: '<reddit></reddit>',
-    };
+    // myApp.config(config(['$stateProvider', '$locationProvider', '$urlRouterProvider']));
+    // function config($stateProvider, $locationProvider, $urlRouterProvider) {
+    //   $locationProvider
+    //   .html5Mode(true) // enable html5Mode for pushstate ('#'-less URLs DOESN'T WORK)
+    //   .hashPrefix('!');
+    myApp.config(config);
 
-    const helloState = {
-      name: 'hello',
-      url: '/posts/:id/edit',
-      template: '<edit></edit>',
-      // controller: editController($stateParams)
-    };
+    function config($stateProvider, $locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false,
+        });
+        const reddit = {
+            name: 'reddit',
+            url: '/',
+            template: '<reddit></reddit>',
+        };
+
+        const helloState = {
+            name: 'hello',
+            url: '/posts/:id/edit',
+            template: '<edit></edit>',
+            // controller: editController($stateParams)
+        };
 
 
-    $stateProvider.state(helloState);
-    $stateProvider.state(reddit);
-  }
+        $stateProvider.state(helloState);
+        $stateProvider.state(reddit);
+    }
 }());
--- public/app.js
+++ public/app.js
@@ -1,24 +1,24 @@
 // BUGS: it doesn't update dynamically... requires to click.
 
 
-(function () {
-  angular.module('app', ['ui.router'])
-  .config(config);
+(function() {
+    angular.module('app', ['ui.router'])
+        .config(config);
 
-  function config($stateProvider) {
-    const editPost = {
-      name: 'editPost',
-      url: '/',
-      template: '<house-new></house-new>',
-    };
+    function config($stateProvider) {
+        const editPost = {
+            name: 'editPost',
+            url: '/',
+            template: '<house-new></house-new>',
+        };
 
-    const home = {
-      name: 'home',
-      url: '/',
-      template: '<reddit></reddit>',
-    };
+        const home = {
+            name: 'home',
+            url: '/',
+            template: '<reddit></reddit>',
+        };
 
-    $stateProvider.state(home);
-    $stateProvider.state(editPost);
-  }// end of config function
+        $stateProvider.state(home);
+        $stateProvider.state(editPost);
+    } // end of config function
 }());
